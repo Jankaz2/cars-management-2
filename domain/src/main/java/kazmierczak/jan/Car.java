bfa87fa255651177cadbfbbf1510bce1a6f34522
jankaz <jk@wp.pl>
2021-03-21 23:07:50 +0100

    Create  withBodyFromArgumentAndInRange and sortByEngineType methods and hasPriceInRange, equalsCarBodyType,  equalsEngineType methods

--- domain/src/main/java/kazmierczak/jan/Car.java
+++ domain/src/main/java/kazmierczak/jan/Car.java
@@ -1,5 +1,7 @@
 package kazmierczak.jan;
 
+import kazmierczak.jan.types.CarBodyType;
+import kazmierczak.jan.types.EngineType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
@@ -18,4 +20,32 @@ public class Car {
     Engine engine;
     CarBody carBody;
     Wheel wheel;
+
+    /**
+     *
+     * @param fromPrice - minimum price in range we are looking for cars
+     * @param toPrice - maximum price in range we are looking for cars
+     * @return true if checking Car's price is in range <fromPrice; toPrice>
+     */
+    public boolean hasPriceInRange(BigDecimal fromPrice, BigDecimal toPrice) {
+        return price.compareTo(fromPrice) >= 0 && price.compareTo(toPrice) <= 0;
+    }
+
+    /**
+     *
+     * @param type - car's body type
+     * @return true if checking Car's body type is equal to body type from arguments
+     */
+    public boolean equalsCarBodyType(CarBodyType type) {
+        return carBody.type.equals(type);
+    }
+
+    /**
+     *
+     * @param engineType - car's engine type
+     * @return true if checking Car's engine type is equal to engine type from arguments
+     */
+    public boolean equalsEngineType(EngineType engineType) {
+        return engine.type.equals(engineType);
+    }
 }
--- service/src/main/java/kazmierczak/jan/CarsService.java
+++ service/src/main/java/kazmierczak/jan/CarsService.java
@@ -2,12 +2,15 @@ package kazmierczak.jan;
 
 import kazmierczak.jan.converter.CarsListJsonConverter;
 import kazmierczak.jan.exception.CarsServiceException;
+import kazmierczak.jan.types.CarBodyType;
+import kazmierczak.jan.types.EngineType;
 import kazmierczak.jan.types.SortItem;
 import kazmierczak.jan.validator.generic.Validator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -50,5 +53,50 @@ public class CarsService {
             Collections.reverse(sortedCars);
         }
         return sortedCars;
+    }
+
+    /**
+     * @param bodyType car's body type
+     * @param fromPrice minimin value of price range
+     * @param toPrice maximum value of price range
+     * @return list of cars which body type is equal to bodyType param and price is
+     * in range <fromPrice; toPrice>
+     */
+    public List<Car> withBodyFromArgumentAndInRange(CarBodyType bodyType, BigDecimal fromPrice, BigDecimal toPrice) {
+        if (fromPrice == null) {
+            throw new CarsServiceException("FromPrice is null");
+        }
+
+        if (toPrice == null) {
+            throw new CarsServiceException("ToPrice is null");
+        }
+
+        if (bodyType == null) {
+            throw new CarsServiceException("BodyType is null");
+        }
+
+        if (fromPrice.compareTo(toPrice) > 0) {
+            throw new CarsServiceException("Wrong range, minimum cannot be greater than maximum");
+        }
+
+        return cars
+                .stream()
+                .filter(car -> car.equalsCarBodyType(bodyType) && car.hasPriceInRange(fromPrice, toPrice))
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * @param engineType type of car's engine
+     * @return list of cars sorted by param engine type
+     */
+    public List<Car> sortByEngineType(EngineType engineType) {
+        if (engineType == null) {
+            throw new CarsServiceException("Enginetype object is null");
+        }
+        return cars
+                .stream()
+                .filter(car -> car.equalsEngineType(engineType))
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
