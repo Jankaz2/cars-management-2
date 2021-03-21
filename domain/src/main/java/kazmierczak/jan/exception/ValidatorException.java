4fd0ab87baeeb3965be0799da6c784afb889aa47
jankaz <jk@wp.pl>
2021-03-21 14:22:43 +0100

    Create generic Validator and ValidatorException

---
+++ domain/src/main/java/kazmierczak/jan/exception/ValidatorException.java
@@ -0,0 +1,7 @@
+package kazmierczak.jan.exception;
+
+public class ValidatorException extends RuntimeException{
+    public ValidatorException(String message) {
+        super(message);
+    }
+}
---
+++ domain/src/main/java/kazmierczak/jan/validator/generic/Validator.java
@@ -0,0 +1,23 @@
+package kazmierczak.jan.validator.generic;
+
+import kazmierczak.jan.exception.ValidatorException;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public interface Validator<T> {
+    Map<String, String> validate(T item);
+
+    static <T> T validate(Validator<T> validator, T item) {
+        var errors = validator.validate(item);
+        if (!errors.isEmpty()) {
+            var message = errors
+                    .entrySet()
+                    .stream()
+                    .map(e -> e.getKey() + " " + e.getValue())
+                    .collect(Collectors.joining(", "));
+            throw new ValidatorException("Validation errors: " + message);
+        }
+        return item;
+    }
+}
