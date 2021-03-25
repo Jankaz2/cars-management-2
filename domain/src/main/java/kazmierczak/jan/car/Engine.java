f1a3f88e00e366ed8da0321f31853f2df50af252
jankaz <jk@wp.pl>
2021-03-25 20:45:49 +0100

    Add ToString methods

--- domain/src/main/java/kazmierczak/jan/car/Engine.java
+++ domain/src/main/java/kazmierczak/jan/car/Engine.java
@@ -1,10 +1,7 @@
 package kazmierczak.jan.car;
 
 import kazmierczak.jan.types.EngineType;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -12,6 +9,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @EqualsAndHashCode
 @Builder
+@ToString
 public class Engine {
     EngineType type;
     BigDecimal power;
