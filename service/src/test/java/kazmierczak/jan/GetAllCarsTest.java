0fd03e4357403f34a1c40549135a763a752e9981
jankaz <jk@wp.pl>
2021-03-25 15:42:11 +0100

    Create test for getAllCars method

---
+++ service/src/test/java/kazmierczak/jan/GetAllCarsTest.java
@@ -0,0 +1,42 @@
+package kazmierczak.jan;
+
+import kazmierczak.jan.car.CarUtils;
+import kazmierczak.jan.config.AppSpringConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class GetAllCarsTest {
+    private CarsService carsService;
+
+    @BeforeEach
+    public void setup() {
+        var context = new AnnotationConfigApplicationContext();
+        context.getEnvironment().setActiveProfiles("test");
+        context.register(AppSpringConfig.class);
+        context.refresh();
+        carsService = context.getBean("carsService", CarsService.class);
+    }
+
+    @Test
+    @DisplayName("when getAllCars method works correctly")
+    public void test1() {
+        var expectedResult = List.of(
+                "AUDI", "FIAT", "PEUGEOT"
+        );
+        var result = carsService.getAllCars()
+                .stream()
+                .map(CarUtils.toModel)
+                .collect(Collectors.toList());
+
+        assertThat(result)
+                .hasSize(3)
+                .containsExactlyElementsOf(expectedResult);
+    }
+}
